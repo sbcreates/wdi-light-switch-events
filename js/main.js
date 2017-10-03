68014673b47cd101012016e06e846ba50e652dfe
sbcreates <drawsarahb@gmail.com>
2017-10-03 19:29:24 -0400

    Created function so light turns on/off on click

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
+var lightSwitch = document.querySelector('.switch')
+var statusText = document.querySelector('.status')
+var body = document.querySelector('body')
 
-
+lightSwitch.addEventListener('click', function(){
+  if (lightSwitch.className === 'switch on'){
+    lightSwitch.className = 'switch off';
+    statusText.innerText = 'Hey, who turned off the lights?';
+    body.className = 'dark';
+  } else {
+    lightSwitch.className = 'switch on';
+    statusText.innerText = "It's so bright in here!";
+    body.className = 'light';
+  }
+})
